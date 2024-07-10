46cdf74a4592c28b8b58fd94a94826961e51a12c
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-10 09:37:06 -0300

    1-commit

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,11 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        var addressFile = "contas.txt";
+        var flowFile = new FileStream(addressFile, FileMode.Open);
 
-       
+        var buffer = new byte[1024];
+
+        flowFile.Read(buffer, 0, 1024);
+
+        Console.ReadLine();
     }
 
-    private static object ContaCorrente(int v1, int v2)
+    private static object CurrentAccount(int v1, int v2)
     {
         throw new NotImplementedException();
     }
