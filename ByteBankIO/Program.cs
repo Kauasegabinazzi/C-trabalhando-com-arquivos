6f3795ee9191b7f80f541203c3f428915fb16992
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-10 09:10:53 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBankIO/Program.cs
@@ -0,0 +1,15 @@
+﻿using ByteBankIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+       
+    }
+
+    private static object ContaCorrente(int v1, int v2)
+    {
+        throw new NotImplementedException();
+    }
+}
