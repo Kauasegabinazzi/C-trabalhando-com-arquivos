b46910d980ad9f5dbf6b9c49ed6f054f259f7887
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-11 19:10:06 -0300

    8-commit

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,6 +5,25 @@ partial class Program
 {
     static void Main(string[] args)
     {
-            Console.ReadLine();
+        var fileAdress = "contas.txt";
+
+        using(var flowFile = new FileStream(fileAdress,FileMode.Open))
+        {
+            var reader = new StreamReader(flowFile);
+
+            //var line = reader.ReadLine();
+
+            //var text = reader.ReadToEnd();
+
+            //int number = reader.Read();
+
+            while (!reader.EndOfStream) 
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
         }
+
+        Console.ReadLine();
     }
+}
